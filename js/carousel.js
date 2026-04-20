b811c534ae1533f73abce1f33014b0aa3c32eb6a
gotorfi <gotorfi@gmail.com>
2026-04-20 19:26:56 +0300

    fix4

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,6 @@
 const data = [
-  { title: "RUINED.", img: "imgs/RUINED.png", info: "RUINED. is a horror game created by GotorFI and produced by GotorFI's company, ShrooMush Studios. RUINED. was released in 2026 on the Roblox platform." },
-  { title: "BaldJumper", img: "imgs/baldjumper.png", info: "Bald Jumper is a platformer game created by GotorFI using Python and Pygame, where the goal is to complete levels. The game was made as an exercise, which is why it is not available." }
+  { title: "RUINED.", img: "/imgs/RUINED.png", info: "RUINED. is a horror game created by GotorFI and produced by GotorFI's company, ShrooMush Studios. RUINED. was released in 2026 on the Roblox platform." },
+  { title: "BaldJumper", img: "/imgs/baldjumper.png", info: "Bald Jumper is a platformer game created by GotorFI using Python and Pygame, where the goal is to complete levels. The game was made as an exercise, which is why it is not available." }
 ];
 
 let index = 0;
