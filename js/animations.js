1d65541b8345b4d657c88095819197919e0ff8fd
gotorfi <gotorfi@gmail.com>
2026-05-22 16:37:43 +0300

    Roblox v1

--- js/animations.js
+++ js/animations.js
@@ -11,6 +11,35 @@ document.addEventListener("DOMContentLoaded", () => {
         threshold: 0.1
     });
 
-    const hiddenElements = document.querySelectorAll('.section1, .section2, .section3, .hero h1, .hero h2, .game-carousel');
+    const hiddenElements = document.querySelectorAll('.section1, .section2, .section3, .section4, .hero h1, .hero h2, .game-carousel, .fadeleft, .faderight');
     hiddenElements.forEach((el) => observer.observe(el));
 });
+
+
+function showVideo(button) {
+    const card = button.closest(".showcase-card");
+
+    const img = card.querySelector("img.showcase-media");
+    const video = card.querySelector("video.showcase-media");
+
+    if (!video) return;
+
+    img.classList.remove("active-media");
+    video.classList.add("active-media");
+
+    video.play();
+}
+
+function showImage(button) {
+    const card = button.closest(".showcase-card");
+
+    const img = card.querySelector("img.showcase-media");
+    const video = card.querySelector("video.showcase-media");
+
+    img.classList.add("active-media");
+
+    if (video) {
+        video.classList.remove("active-media");
+        video.pause();
+    }
+}
